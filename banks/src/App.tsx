68c752da2fdc47350367c45b01775df9666dba43
takumu <takumu155695@gmail.com>
2022-08-14 11:42:13 +0900

    初めての useReducer

---
+++ banks/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Bank } from './pages/bank/Bank'
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h2># useReducer</h2>
+      <Bank />
+    </div>
+  );
+}
+
+export default App;
